a8fa1fe60838dd3074550703bbbd459870dcf5db
JeoftonCosta <jeofton_costa@hotmail.com>
2023-08-23 11:06:38 -0300

    Exemplos de condicionais e repetição

---
+++ index.js
@@ -0,0 +1,49 @@
+var time = "corinthians";
+
+// var, let e const
+/**
+ *  let e const possuem escopo local
+ * 
+ */
+// let outracoisa;
+//Estrutura de decisão simples
+// if(flamengo == "Foi gol"){
+// let algumacoisa;
+//   console.log("A partida é nossa\n")
+// }
+
+/**
+ * Decisões compostas
+ * 
+ */
+
+// if(time == "flamengo"){
+//   console.log("Aha! Uhu! O juiz é nosso.")
+// }else{
+//   console.log("Au!Au!Au! Palmeiras não tem mundial.")
+// }
+
+// Multiplas escolha
+
+// if(time == "flamengo"){
+//   console.log("Aha! Uhu! O juiz é nosso.")
+// }else if(time == 'palmeiras'){
+//   console.log("Au!Au!Au! Palmeiras não tem mundial.")
+// }else{
+//   console.log("Prefiro o Minas Clube.")
+// }
+
+// estrutura for
+
+// for(let i = 10; i >= 1; i--){
+//   console.log(`Valor de i na ${i}ª volta`);
+// }
+
+// estrutura while
+
+let i = 0;
+
+while(i <= 5){
+  console.log(i)
+  i++
+}
